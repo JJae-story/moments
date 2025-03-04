5819c0226dd567ede80e8cab82c61ad5e8dce284
JJae <jjae.3.lee@gmail.com>
2025-03-04 22:26:18 +0900

    refactor : 리뷰 반영
    - Get, Delete Mapping 에서 imageId 를 쿼리 파라미터에서 PathVariable로 변경

--- src/main/java/com/uijae/moments/album/s3/controller/AwsS3Controller.java
+++ src/main/java/com/uijae/moments/album/s3/controller/AwsS3Controller.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +23,7 @@ public class AwsS3Controller {
 
   // 이미지 업로드
   @Operation(summary = "이미지 업로드", description = "여러 개의 파일을 업로드하고, 업로드된 이미지 ID 목록을 반환합니다.")
-  @PostMapping("/upload")
+  @PostMapping("/files")
   public ResponseEntity<List<Long>> uploadFiles(@RequestParam("files") List<MultipartFile> files,
       @RequestParam("albumId") Long albumId) {
 
@@ -33,16 +34,16 @@ public class AwsS3Controller {
 
   // 이미지 URL 조회
   @Operation(summary = "이미지 다운로드", description = "이미지 ID를 통해 S3 URL을 가져옵니다.")
-  @GetMapping("/download")
-  public ResponseEntity<String> getFileUrl(@RequestParam("imageId") Long imageId) {
+  @GetMapping("/{imageId}")
+  public ResponseEntity<String> getFileUrl(@PathVariable Long imageId) {
 
     return ResponseEntity.ok(awsS3Service.getFileUrl(imageId));
   }
 
   // 이미지 삭제
   @Operation(summary = "이미지 삭제", description = "이미지 ID를 이용해 S3에서 파일을 삭제합니다.")
-  @DeleteMapping("/delete")
-  public ResponseEntity<Void> deleteFile(@RequestParam("imageId") Long imageId) {
+  @DeleteMapping("/{imageId}")
+  public ResponseEntity<Void> deleteFile(@PathVariable Long imageId) {
 
     awsS3Service.deleteFile(imageId);
 
